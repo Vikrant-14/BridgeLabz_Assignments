5c8bf0967561ca4d94535b17fe047f1e01c9bf9a
Vikrant-14 <vikrantgawale123@gmail.com>
2024-04-19 17:56:43 +0530

    [Vikrant]Add : LineComparison file added with display message

---
+++ LineComparsion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class LineComparison{
+    static void Display()
+    {
+        Console.WriteLine("Welcome to Line Comparison Computation Program!!!");
+    }
+    static void Main()
+    {
+        Display();
+    }
+}
