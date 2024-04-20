a9b4101863aa0526ae22dc651a6d4679244a9ada
Vikrant-14 <vikrantgawale123@gmail.com>
2024-04-20 15:13:20 +0530

    [Vikrant]Add : Random dice number function added

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -1,23 +1,33 @@
-﻿using SnakeAndLadder;
+﻿
 using System;
 
 namespace SnakeLadder
 {
-    internal class Game
+    internal class Player
     {
         static int start = 0;
         static int end = 100;
 
+        public string? Name { get; set; }
         int initialPos;
-        Game()
+
+        Player()
         {
             initialPos = 0;
         }
 
-       
+        public int RandomDice()
+        {
+            Random rnd = new Random();
+            int dice = rnd.Next(1, 7);
+
+            return dice;
+        }
+
         static void Main(string[] args)
         {
             Player player1 = new Player();
+            Console.WriteLine(player1.RandomDice());
         }
-    } 
+    }
 }
