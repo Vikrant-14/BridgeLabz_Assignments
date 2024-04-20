7ce88d8bdcdac7d7d797be4803d83eb3c5f48d6f
Vikrant-14 <vikrantgawale123@gmail.com>
2024-04-20 14:32:55 +0530

    [Vikrant]Add : created a player class and set his initial position to zero

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SnakeAndLadder;
+using System;
+
+namespace SnakeLadder
+{
+    internal class Game
+    {
+        static int start = 0;
+        static int end = 100;
+
+        int initialPos;
+        Game()
+        {
+            initialPos = 0;
+        }
+
+       
+        static void Main(string[] args)
+        {
+            Player player1 = new Player();
+        }
+    } 
+}
