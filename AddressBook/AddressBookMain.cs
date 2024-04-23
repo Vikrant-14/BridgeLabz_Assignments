98184d001851af85a94d1ed127122496ce665c29
Vikrant-14 <vikrantgawale123@gmail.com>
2024-04-23 15:37:16 +0530

    [Vikrant] Refactor : Updated .gitignore file

---
+++ AddressBook/AddressBookMain.cs
@@ -0,0 +1,23 @@
+﻿using AddressBook;
+using System;
+
+namespace Assignment3
+{
+    class AddressBookMain
+    {
+        List<Contact>? ContactList;
+
+        static AddressBookMain()
+        {
+            Console.WriteLine("================================");
+            Console.WriteLine("Welcome to Address Book Program");
+            Console.WriteLine("================================\n");
+        }
+
+        public static void Main()
+        {
+            Contact contact = new Contact();    
+            Console.WriteLine(contact.FirstName);
+        }
+    }
+}
